b23d22579d190a17f8f9bc7870fb10dc15e12a43
JT07616 <juraj.tojcic@student.unipu.hr>
2026-05-17 09:45:13 +0200

    Dodana inicijalna Spring Boot struktura

---
+++ src/main/java/hr/fipu/knjiznica/KnjiznicaSpringApplication.java
@@ -0,0 +1,13 @@
+package hr.fipu.knjiznica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnjiznicaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnjiznicaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/fipu/knjiznica/KnjiznicaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fipu.knjiznica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnjiznicaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
