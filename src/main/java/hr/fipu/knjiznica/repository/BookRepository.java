4f4d7162bc6aab44e6a5c002bb40ef5acba10eb5
JT07616 <juraj.tojcic@student.unipu.hr>
2026-05-18 00:11:01 +0200

    Dodani repository interface-i

---
+++ src/main/java/hr/fipu/knjiznica/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package hr.fipu.knjiznica.repository;
+
+import hr.fipu.knjiznica.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+
+    List<Book> findByGenreContainingIgnoreCase(String genre);
+
+    List<Book> findByAvailableTrue();
+}
---
+++ src/main/java/hr/fipu/knjiznica/repository/LoanRepository.java
@@ -0,0 +1,15 @@
+package hr.fipu.knjiznica.repository;
+
+import hr.fipu.knjiznica.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LoanRepository extends JpaRepository<Loan, Integer> {
+
+    List<Loan> findByStatus(String status);
+
+    List<Loan> findByBookId(Integer bookId);
+
+    List<Loan> findByMemberId(Integer memberId);
+}
