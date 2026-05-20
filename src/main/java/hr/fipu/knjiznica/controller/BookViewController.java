0e42fa65c2f96170d40b465e1479fac190428213
JT07616 <juraj.tojcic@student.unipu.hr>
2026-05-21 01:01:22 +0200

    Dodane Thymeleaf forme za knjige

--- src/main/java/hr/fipu/knjiznica/controller/BookViewController.java
+++ src/main/java/hr/fipu/knjiznica/controller/BookViewController.java
@@ -1,10 +1,14 @@
 package hr.fipu.knjiznica.controller;
 
+import hr.fipu.knjiznica.dto.BookRequest;
+import hr.fipu.knjiznica.model.Book;
 import hr.fipu.knjiznica.service.BookService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class BookViewController {
@@ -26,4 +30,97 @@ public class BookViewController {
         model.addAttribute("book", bookService.findById(id));
         return "books/details";
     }
+
+    @GetMapping("/books/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("bookForm", new BookRequest());
+        model.addAttribute("formTitle", "Dodaj knjigu");
+        model.addAttribute("formAction", "/books");
+        return "books/form";
+    }
+
+    @PostMapping("/books")
+    public String create(
+            @Valid @ModelAttribute("bookForm") BookRequest form,
+            BindingResult bindingResult,
+            Model model,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("formTitle", "Dodaj knjigu");
+            model.addAttribute("formAction", "/books");
+            return "books/form";
+        }
+
+        Book book = new Book(
+                form.getTitle(),
+                form.getAuthor(),
+                form.getGenre(),
+                form.getIsbn(),
+                form.getPublishedYear()
+        );
+
+        bookService.create(book);
+        redirectAttributes.addFlashAttribute("message", "Knjiga je uspjesno dodana.");
+
+        return "redirect:/books";
+    }
+
+    @GetMapping("/books/{id}/edit")
+    public String showEditForm(@PathVariable Integer id, Model model) {
+        Book book = bookService.findById(id);
+
+        BookRequest form = new BookRequest();
+        form.setTitle(book.getTitle());
+        form.setAuthor(book.getAuthor());
+        form.setGenre(book.getGenre());
+        form.setIsbn(book.getIsbn());
+        form.setPublishedYear(book.getPublishedYear());
+
+        model.addAttribute("bookForm", form);
+        model.addAttribute("bookId", id);
+        model.addAttribute("formTitle", "Uredi knjigu");
+        model.addAttribute("formAction", "/books/" + id);
+
+        return "books/form";
+    }
+
+    @PostMapping("/books/{id}")
+    public String update(
+            @PathVariable Integer id,
+            @Valid @ModelAttribute("bookForm") BookRequest form,
+            BindingResult bindingResult,
+            Model model,
+            RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bookId", id);
+            model.addAttribute("formTitle", "Uredi knjigu");
+            model.addAttribute("formAction", "/books/" + id);
+            return "books/form";
+        }
+
+        Book book = new Book(
+                form.getTitle(),
+                form.getAuthor(),
+                form.getGenre(),
+                form.getIsbn(),
+                form.getPublishedYear()
+        );
+
+        bookService.update(id, book);
+        redirectAttributes.addFlashAttribute("message", "Knjiga je uspjesno uredjena.");
+
+        return "redirect:/books";
+    }
+
+    @PostMapping("/books/{id}/delete")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        bookService.delete(id);
+        redirectAttributes.addFlashAttribute("message", "Knjiga je uspjesno obrisana.");
+
+        return "redirect:/books";
+    }
+
+
 }
