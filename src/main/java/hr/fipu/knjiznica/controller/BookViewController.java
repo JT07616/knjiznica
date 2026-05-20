58dee8a040f4bef93841e0108cb301976351a050
JT07616 <juraj.tojcic@student.unipu.hr>
2026-05-20 20:06:03 +0200

    Dodan Thymeleaf prikaz detalja knjige

--- src/main/java/hr/fipu/knjiznica/controller/BookViewController.java
+++ src/main/java/hr/fipu/knjiznica/controller/BookViewController.java
@@ -4,6 +4,7 @@ import hr.fipu.knjiznica.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BookViewController {
@@ -18,5 +19,11 @@ public class BookViewController {
     public String list(Model model) {
         model.addAttribute("books", bookService.findAll());
         return "books/list";
+    }
+
+    @GetMapping("/books/{id}")
+    public String details(@PathVariable Integer id, Model model) {
+        model.addAttribute("book", bookService.findById(id));
+        return "books/details";
     }
 }
